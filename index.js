8880e1ac826f1617ba693a2b442fdaaddd236e65
Hoeng Mengdy <your_email@gmail.com>
2026-04-21 15:41:20 +0700

    update css and html

--- index.js
+++ index.js
@@ -10,20 +10,19 @@ let header = document.querySelector('.header')
 let toggleMenu = document.querySelector('.bar')
 let menu = document.querySelector('nav ul')
 
-const toggle = (e)=>{
+const toggle = (e) => {
     toggleMenu.classList.toggle('active')
     menu.classList.toggle('activeMenu')
 }
 
-toggleMenu.addEventListener('click',toggle)
+toggleMenu.addEventListener('click', toggle)
 
 
 
-window.addEventListener('scroll',()=>{
-    if(window.scrollY>50){
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
         header.classList.add('sticky')
-    }
-    else{
+    } else {
         header.classList.remove('sticky')
     }
 })
@@ -38,34 +37,33 @@ window.addEventListener('scroll',()=>{
 
 const apiKey = "29f8e42efe874ee2be23f0d1edb6844b"
 
-const fetchData = async (category,pageSize)=>{
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`
-    const data = await fetch(url)
-    const response = await data.json()
-    console.log(response);
-    return response.articles
-    
-}
-// fetchData('general',5)
+const fetchData = async(category, pageSize) => {
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`
+        const data = await fetch(url)
+        const response = await data.json()
+        console.log(response);
+        return response.articles
+
+    }
+    // fetchData('general',5)
 
 //adding breaking news
 
-const add_breakingNews = (data)=>{
+const add_breakingNews = (data) => {
     breakingImg.innerHTML = `<img src=${data[0].urlToImage} alt="image">`
     breakingNews_title.innerHTML = `<a href=${data[0].url} target="_blank"><h2>${data[0].title}</h2></a>`
     breakingNews_desc.innerHTML = `${data[0].description}`
 }
-fetchData('general',5).then(add_breakingNews)
+fetchData('general', 5).then(add_breakingNews)
 
-const add_topNews = (data)=>{
+const add_topNews = (data) => {
     let html = ''
     let title = ''
-    data.forEach((element)=>{
-        if (element.title.length<100){
+    data.forEach((element) => {
+        if (element.title.length < 100) {
             title = element.title
-        }
-        else{
-            title = element.title.slice(0,100) + "..."
+        } else {
+            title = element.title.slice(0, 100) + "..."
         }
 
         html += `<div class="news">
@@ -81,17 +79,16 @@ const add_topNews = (data)=>{
     })
     topNews.innerHTML = html
 }
-fetchData('general',20).then(add_topNews)
+fetchData('general', 20).then(add_topNews)
 
-const add_sportsNews = (data)=>{
+const add_sportsNews = (data) => {
     let html = ''
     let title = ''
-    data.forEach((element)=>{
-        if (element.title.length<100){
+    data.forEach((element) => {
+        if (element.title.length < 100) {
             title = element.title
-        }
-        else{
-            title = element.title.slice(0,100) + "..."
+        } else {
+            title = element.title.slice(0, 100) + "..."
         }
 
         html += `<div class="newsCard">
@@ -107,16 +104,15 @@ const add_sportsNews = (data)=>{
     })
     sportsNews.innerHTML = html
 }
-fetchData('sports',5).then(add_sportsNews)
-const add_businessNews = (data)=>{
+fetchData('sports', 5).then(add_sportsNews)
+const add_businessNews = (data) => {
     let html = ''
     let title = ''
-    data.forEach((element)=>{
-        if (element.title.length<100){
+    data.forEach((element) => {
+        if (element.title.length < 100) {
             title = element.title
-        }
-        else{
-            title = element.title.slice(0,100) + "..."
+        } else {
+            title = element.title.slice(0, 100) + "..."
         }
 
         html += `<div class="newsCard">
@@ -132,16 +128,15 @@ const add_businessNews = (data)=>{
     })
     businessNews.innerHTML = html
 }
-fetchData('business',5).then(add_businessNews)
-const add_techNews = (data)=>{
+fetchData('business', 5).then(add_businessNews)
+const add_techNews = (data) => {
     let html = ''
     let title = ''
-    data.forEach((element)=>{
-        if (element.title.length<100){
+    data.forEach((element) => {
+        if (element.title.length < 100) {
             title = element.title
-        }
-        else{
-            title = element.title.slice(0,100) + "..."
+        } else {
+            title = element.title.slice(0, 100) + "..."
         }
 
         html += `<div class="newsCard">
@@ -157,4 +152,38 @@ const add_techNews = (data)=>{
     })
     techNews.innerHTML = html
 }
-fetchData('technology',5).then(add_techNews)
+
+fetchData('technology', 5).then(add_techNews)
+const genz = document.getElementById("GenZ");
+const popup = document.getElementById("popup");
+const popupImg = document.getElementById("popupImg");
+const closeBtn = document.getElementById("close");
+
+// CLICK IMAGE
+genz.addEventListener("click", function() {
+    // zoom effect
+    this.classList.toggle("zoom");
+
+    // show popup
+    popup.style.display = "flex";
+
+    // set image inside popup
+    popupImg.src = this.querySelector("img").src;
+});
+
+// CLOSE POPUP
+closeBtn.addEventListener("click", function() {
+    popup.style.display = "none";
+});
+
+// CLICK OUTSIDE CLOSE
+popup.addEventListener("click", function(e) {
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+});
+
+// GO TO PAGE
+function goPage() {
+    window.location.href = "genz.html"; // change link
+}
